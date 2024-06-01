68bc942d4edb3b9b7a8ddb4acf5e3e53dabbca52
Muneeb Shahid <muneeb.si2002@gmail.com>
2024-06-01 23:05:48 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+# Write a program to check whether a person is eligible to vote or not?
+# voting_age = 18
+# age = int(input("Enter your age: "))
+
+# if age >= voting_age:
+#     print("Eligible to vote")
+# else:
+#     print("Not eligible to vote")
+    
+# Write a program to check char is vowel or not.
+# char = input("Enter a character: ")
+
+# if char in 'aeiouAEIOU':
+#     print("Vowel")
+# else:
+#     print("Consonant")
+ 
+        
+# Write a program to check the number is positive or negative. User input.
+# num = int(input("Enter a number: "))
+
+# if num > 0:
+#     print("Positive")
+# elif num < 0:
+#     print("Negative")
+# else:
+#     print("Zero")
+
+# Write a program to check whether a number is odd or even?
+
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# Write a program to display the grade of the user in subject A, ask user marks obtained out of 100
+# marks = int(input("Enter your marks in subject A (out of 100): "))
+
+# if marks >= 90:
+#     print("Grade: A")
+# elif marks >= 80:
+#     print("Grade: B")
+# elif marks >= 70:
+#     print("Grade: C")
+# elif marks >= 60:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+
+# Write a program to check whether a number is divisible by 7
+# number = int(input("Enter a number: "))
+
+# if number % 7 == 0:
+#     print(f"{number} is divisible by 7")
+# else:
+#     print(f"{number} is not divisible by 7")
+
+
+# Write a program to check if year is leap year.
+# year = int(input("Enter a year: "))
+# if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+#     print(f"{year} is a leap year")
+# else:
+#     print(f"{year} is not a leap year")
+
+# Write a program to ask user its name and check whether name consists of 5 or more letters
+
